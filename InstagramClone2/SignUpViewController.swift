67e7272012d0847e1846c5bd5375425ee72a4c6a
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2018-05-07 22:28:10 +0800

    AutoLayout

---
+++ InstagramClone2/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  InstagramClone2
+//
+//  Created by Francis Jemuel Bergonia on 05/05/2018.
+//  Copyright © 2018 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismiss_OnClick(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
